1f13a161ce0699a58f0aae0c4de1636808f876e1
Aleksandra Los <oluchna@hotmail.com>
2022-06-01 22:06:04 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import BeerList from './components/BeerList';
+import { useEffect, useState } from 'react';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [beer, setBeer] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.punkapi.com/v2/beers')
+      .then(res => {
+        return res.json();
+      })
+      .then(beer =>{ 
+        console.log(beer);
+        setBeer(beer)});
+
+}, [])
+
+return (
+  <>
+    <Navbar />
+    <main>
+      <h1>Beer</h1>
+      <h2>Because you can't drink bacon.</h2>
+    </main>
+  { beer && <BeerList beer={beer} />}
+  <Footer/>
+  </>
+);
 }
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return ( <>
+    <div class="card text-center">
+  <div class="card-footer text-muted">
+  <p class="copyright">© Aleksandra Los, 2022</p>
+  </div>
+</div>
+    </> );
+}
+ 
+export default Footer;
